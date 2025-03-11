fabdaf9810ebd0263574722b523070933f8b08a1
TopPumaUma <164445156+TopPumaUma@users.noreply.github.com>
2025-03-10 17:54:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Form submitted!');
 });
+
+
+const gamesButton = document.getElementById("games-button");
+const gamesGrid = document.getElementById("games-grid");
+
+gamesButton.addEventListener("click", () => {
+  gamesGrid.classList.toggle("hidden");
+});
