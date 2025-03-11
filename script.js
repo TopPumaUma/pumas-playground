ac67ad0af0500aae055a2b8de24385fc56870061
TopPumaUma <164445156+TopPumaUma@users.noreply.github.com>
2025-03-10 18:00:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ window.addEventListener("scroll", () => {
     footer.classList.add("hidden"); // Hide the footer
   }
 });
+
+
+const footer = document.getElementById("footer-note");
+
+window.addEventListener("scroll", () => {
+  // Check if the user is near the bottom of the page
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+    footer.classList.remove("hidden"); // Show the footer
+  } else {
+    footer.classList.add("hidden"); // Hide the footer
+  }
+});
