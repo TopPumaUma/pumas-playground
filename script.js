bb0d25e217e4ba7533c88f4429eef9038d120654
TopPumaUma <164445156+TopPumaUma@users.noreply.github.com>
2025-03-10 17:59:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ const gamesGrid = document.getElementById("games-grid");
 gamesButton.addEventListener("click", () => {
   gamesGrid.classList.toggle("hidden");
 });
+
+
+const footer = document.getElementById("footer-note");
+
+window.addEventListener("scroll", () => {
+  // Check if the user is near the bottom of the page
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+    footer.classList.remove("hidden"); // Show the footer
+  } else {
+    footer.classList.add("hidden"); // Hide the footer
+  }
+});
